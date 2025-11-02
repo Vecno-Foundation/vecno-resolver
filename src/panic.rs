145e24816e737f9edbe178c7165d3b99266fb427
Yooshikii <knut-a_91@hotmail.com>
2025-11-02 01:46:16 +0100

    Updated panic_handler.

--- src/panic.rs
+++ src/panic.rs
@@ -2,9 +2,11 @@ use std::panic;
 
 pub fn init_ungraceful_panic_handler() {
     let default_hook = panic::take_hook();
+
     panic::set_hook(Box::new(move |panic_info| {
         default_hook(panic_info);
-        println!("Exiting...");
-        std::process::exit(1);
+
+        eprintln!("PANIC RECOVERED: The process continues running despite a thread panic.");
+        eprintln!("   This is expected in fault-tolerant mode. Check logs for details.");
     }));
 }
